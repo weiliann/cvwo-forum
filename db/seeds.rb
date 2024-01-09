2377b7b8191046887f77477eeac294b3cd6c6738
Wei Lian <weilian2003@live.com>
2024-01-09 17:27:16 +0800

    Init faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+n = 10
+n.times do
+  u = User.create(name: Faker::Name)
+  p = u.posts.create(
+    title: Faker::Lorem.paragraph,
+    body: Faker::Lorem.paragraph,
+    category: Faker::Lorem.word
+  )
+  p.comments.create(
+    body: Faker::Lorem.paragraph,
+    user_id: Faker::Number.between(from: 1, to: n)
+  )
+end
