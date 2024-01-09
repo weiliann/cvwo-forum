41d7b213b332d85101710f9ab5a32829912359b6
Wei Lian <weilian2003@live.com>
2024-01-09 16:50:39 +0800

    Init comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_09_084520) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_09_084842) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,5 +35,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_09_084520) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
