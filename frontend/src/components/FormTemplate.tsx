cb0788d5a24f5cfe815069ad6ec73344d8c7be2f
Wei Lian <weilian2003@live.com>
2024-01-15 19:24:42 +0800

    Remove the hard coded user_id field for forms

--- frontend/src/components/FormTemplate.tsx
+++ frontend/src/components/FormTemplate.tsx
@@ -11,6 +11,7 @@ type FormParams = {
 }
 
 export default function FormTemplate({ errors, fields }: FormParams) {
+  const user_id = sessionStorage.getItem("user_id")
   return (
     <>
       <p>
@@ -29,8 +30,8 @@ export default function FormTemplate({ errors, fields }: FormParams) {
         {isObj(errors) && <span>{errors.category}</span>}
       </p>
       <p>
-        <input type={fields ? "hidden" : "text"} name="user_id" placeholder="temp soln for user_id" value={fields?.user_id}/>
-        {isObj(errors) && <span>{errors.user_id}</span>}
+        <input type="hidden" name="user_id" value={user_id || ""} />
+        {user_id ? '' : <span>Must be logged in to create post</span> }
       </p> 
 
       <button type="submit">Submit</button>
--- frontend/src/components/NewCommentForm.tsx
+++ frontend/src/components/NewCommentForm.tsx
@@ -2,17 +2,17 @@ import { useFetcher } from "react-router-dom"
 
 export default function NewCommentForm() {
   const fetcher = useFetcher();
-  return (
-    <>
-      <fetcher.Form method="POST" action="newComment">
-        <label htmlFor="comment-body">Write your comments: </label>
-        <input type="text" name="body" id="comment-body" />
-        <br />
-        <label htmlFor="comment-user">temp soln for user id</label>
-        <input type="text" name="user_id" id="comment-user" defaultValue={1} />
+  const user_id = sessionStorage.getItem("user_id");
+  const commentForm = (
+    <fetcher.Form method="POST" action="newComment">
+    <label htmlFor="comment-body">Write your comments: </label>
+    <input type="text" name="body" id="comment-body" />
+    <br />
+    <label htmlFor="comment-user">temp soln for user id</label>
+    <input type="text" name="user_id" id="comment-user" defaultValue={1} />
 
-        <button type="submit">Post comment</button>
-      </fetcher.Form> 
-    </>
+    <button type="submit">Post comment</button>
+  </fetcher.Form> 
   )
+  return (user_id ? commentForm : "Must be logged in to comment")
 }
