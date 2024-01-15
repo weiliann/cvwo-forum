4d76db3142bf93423ada53783ead861578e39059
Wei Lian <weilian2003@live.com>
2024-01-15 21:53:52 +0800

    Add category filtering function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,26 @@ import PostResponse from "./types/PostResponse";
 import PostCard from "./components/PostCard"
 import "./App.css"
 import NavBar from "./components/NavBar";
+import { useState } from "react";
 
 
 function App() {
   const allPosts = useLoaderData() as PostResponse[];
+  const [searchCategory, setSearchCategory] = useState('');
   return (
     <>
       <NavBar />
       <div className="body">
-        {allPosts.map(post => (
-          <PostCard key={post.id} post_id={post.id} title={post.title} name={post.user.name}/>
-        ))}
+        <input
+          type="text"
+          placeholder="filter category"
+          value={searchCategory}
+          onChange={e => setSearchCategory(e.target.value)}
+        />
+        {allPosts.filter(post => post.category.includes(searchCategory))
+          .map(post => (
+            <PostCard key={post.id} post_id={post.id} title={post.title} name={post.user.name} />
+          ))}
       </div>
     </>
   )
