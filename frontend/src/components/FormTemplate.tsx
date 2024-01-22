d717dbe64c0c7471acfb36e0012a2ee3b7a63ed1
Wei Lian <weilian2003@live.com>
2024-01-22 17:35:42 +0800

    Add MUI components for new post page

--- frontend/src/components/FormTemplate.tsx
+++ frontend/src/components/FormTemplate.tsx
@@ -1,4 +1,6 @@
+import { Box, Button, Container, Stack, TextField, Typography } from "@mui/material";
 import { PostData } from "../actions/postActions";
+import { ThemeContext } from "@emotion/react";
 
 type FormParams = {
   errors: PostData | Response
@@ -13,32 +15,65 @@ type FormParams = {
 export default function FormTemplate({ errors, fields }: FormParams) {
   const user_id = sessionStorage.getItem("user_id")
   return (
-    <>
-      <p>
-        <label htmlFor="post-title">Post title: </label>
-        <input type="text" name="title" id="post-title" defaultValue={fields && fields.title}/>
-        {isObj(errors) && <span>{errors.title}</span>}
-      </p>
-      <p>
-        <label htmlFor="post-body">Post body: </label>
-        <input type="text" name="body" id="post-body" defaultValue={fields && fields.body}/>
-        {isObj(errors) && <span>{errors.body}</span>}
-      </p>
-      <p>
-        <label htmlFor="post-cat">Category: </label>
-        <input type="text" name="category" id="post-cat" defaultValue={fields && fields.category}/>
-        {isObj(errors) && <span>{errors.category}</span>}
-      </p>
-      <p>
-        <input type="hidden" name="user_id" value={user_id || ""} />
-        {user_id ? '' : <span>Must be logged in to create post</span> }
-      </p> 
+    <Container sx={{ mt: "8vh" }}>
+      <Stack spacing={2}>
+        <Stack direction="row" spacing={2} >
+          <TextField
+            id="post-title"
+            label="Post title"
+            name="title"
+            defaultValue={fields && fields.title}
+            variant="outlined"
+            sx={{ display: 'block' }}
+            error={isObj(errors)}
+            helperText={isObj(errors) && errors.title}
+            fullWidth
+          />
+          <TextField
+            id="post-cat"
+            label="Post category"
+            name="category"
+            defaultValue={fields && fields.category}
+            variant="outlined"
+            sx={{ display: 'block' }}
+            error={isObj(errors)}
+            helperText={isObj(errors) && errors.category}
+            fullWidth
+          />
+        </Stack>
+        <TextField
+          id="post-body"
+          label="Post body"
+          name="body"
+          defaultValue={fields && fields.body}
+          variant="outlined"
+          sx={{ display: 'block' }}
+          multiline
+          rows={4}
+          fullWidth
+          error={isObj(errors)}
+          helperText={isObj(errors) && errors.body}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "end",
 
-      <button type="submit">Submit</button>
-    </>
+          }}
+        >
+          <Typography>
+            <input type="hidden" name="user_id" value={user_id || ""} />
+            {user_id ? '' : <Typography color={"error"}>Log in to start posting</Typography>}
+          </Typography>
+          <Button type="submit" variant="contained" >Submit</Button>
+        </Box>
+      </Stack>
+    </Container>
   )
 }
 
+// returns true if errors is an object
 function isObj(errors: PostData | Response): errors is PostData {
   if (errors === undefined) {
     return false;
--- frontend/src/components/SubmitForm.tsx
+++ frontend/src/components/SubmitForm.tsx
@@ -2,15 +2,22 @@ import { Form, useActionData } from "react-router-dom";
 import NavBar from "./NavBar";
 import { PostData } from "../actions/postActions";
 import FormTemplate from "./FormTemplate";
+import { Container } from "@mui/material";
 
 export default function SubmitForm() {
   const errors = useActionData() as PostData | Response
   return (
     <>
       <NavBar />
-      <Form method="post">
-        <FormTemplate errors={errors}/>
-      </Form>
+      <Container
+        sx={{
+          height: "100vh"
+        }}
+      >
+        <Form method="post">
+          <FormTemplate errors={errors} />
+        </Form>
+      </Container>
     </>
   )
 }
