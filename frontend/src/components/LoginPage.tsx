a6a19e199560d8ada6ef930125a77bcedacabe64
Wei Lian <weilian2003@live.com>
2024-01-22 18:39:18 +0800

    Add styles for login page

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,23 +1,54 @@
 import { Form } from "react-router-dom";
 import NavBar from "./NavBar";
+import { Box, Button, Divider, Stack, TextField, Typography } from "@mui/material";
 
 export default function LoginPage() {
   return (
     <>
       <NavBar />
-      <br />
-      <p>Log in to your existing account</p>
-      <Form method="PATCH" action='/login'>
-        <label htmlFor="user-name">Enter your name</label>
-        <input type="text" name="name" id="user-name" />
-        <button type="submit">Enter</button>
-      </Form>
-      <p>Or create a new account below</p>
-      <Form method="POST">
-        <label htmlFor="user-name">Create a new name</label>
-        <input type="text" name="name" id="user-name" />
-        <button type="submit">Enter</button>
-      </Form>
+      <Box
+        sx={{
+          mt: 10,
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+
+        <Stack
+          spacing={3}
+          sx={{ width: "50vw" }}
+        >
+          <Typography >Sign in to existing account</Typography>
+          <Box>
+            <Form
+              method="PATCH"
+              action='/login'
+              style={{ display: "flex", gap: "2rem" }}
+            >
+              <TextField
+                label="Username"
+                name="name"
+                variant="outlined"
+              />
+              <Button variant="contained" type="submit" style={{ alignSelf: "end" }}>Sign In</Button>
+            </Form>
+          </Box>
+          <Divider />
+          <Typography >Or create a new account below</Typography>
+          <Box>
+            <Form method="POST" style={{ display: "flex", gap: "2rem" }}>
+              <TextField
+                label="New username"
+                name="name"
+                variant="outlined"
+              />
+              <Button variant="contained" type="submit" style={{ alignSelf: "end" }}>Register</Button>
+            </Form>
+          </Box>
+        </Stack>
+      </Box>
     </>
   )
 }
