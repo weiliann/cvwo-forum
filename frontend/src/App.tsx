c867b5f32720e5c42554c22b4dc906f87b33619e
Wei Lian <weilian2003@live.com>
2024-01-22 11:54:25 +0800

    Add MUI components for home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,32 @@ import PostCard from "./components/PostCard"
 import "./App.css"
 import NavBar from "./components/NavBar";
 import { useState } from "react";
+import { Box, Container, Stack, TextField } from "@mui/material";
 
-
-function App() {
+export default function App() {
   const allPosts = useLoaderData() as PostResponse[];
   const [searchCategory, setSearchCategory] = useState('');
   return (
     <>
       <NavBar />
-      <div className="body">
-        <input
-          type="text"
-          placeholder="filter category"
-          value={searchCategory}
-          onChange={e => setSearchCategory(e.target.value)}
-        />
-        {allPosts.filter(post => post.category.includes(searchCategory))
-          .map(post => (
-            <PostCard key={post.id} post_id={post.id} title={post.title} name={post.user.name} />
-          ))}
-      </div>
+      <Box sx={{ mt: 3 }}>
+        <Container sx={{ p: 7 }}>
+          <TextField
+            variant="outlined"
+            type="text"
+            placeholder="filter category"
+            value={searchCategory}
+            onChange={e => setSearchCategory(e.target.value)}
+            sx={{ display: 'block', width: '30%', margin: '3rem auto' }}
+          />
+          <Stack spacing={2}>
+            {allPosts.filter(post => post.category.includes(searchCategory))
+              .map(post => (
+                <PostCard key={post.id} post_id={post.id} title={post.title} name={post.user.name} />
+              ))}
+          </Stack>
+        </Container>
+      </Box>
     </>
   )
 }
-
-export default App
--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,19 +1,46 @@
+import { Box, Paper, Typography } from '@mui/material';
 import '../App.css';
 import { Link } from 'react-router-dom';
 
 type PostCardParams = {
   post_id: number,
   title: string,
-  name: string  
+  name: string
 }
 // post cards displayed on index page linking to individual posts
 function PostCard(params: PostCardParams) {
   const { post_id, title, name } = params;
   return (
-    <div className="card">
-      <Link to={`comments/${post_id}`}> {title} </Link> 
-      {post_id + title + name } 
-    </div>
+    <Paper
+      elevation={16}
+      sx={{
+        p: 2,
+        height: '10vh',
+        display: 'flex',
+        flexGrow: 1,
+      }}
+    >
+      <Link
+        to={`comments/${post_id}`}
+        style={{ width: '100%' }}
+      >
+        <Box 
+          sx={{
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+          }}
+        >
+          <Typography variant='h5'>
+            Posted by: {name}
+          </Typography>
+          <Typography variant='h3'>
+            Title: {title}
+          </Typography>
+        </Box>
+      </Link>
+    </Paper>
   )
 }
 
